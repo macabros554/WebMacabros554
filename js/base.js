4bdcae0b098a5244a304ef3219e0402b1eb085c0
Javi Lira <74186769+macabros554@users.noreply.github.com>
2021-07-16 22:03:55 +0200

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,6 +1,16 @@
 function abrirTetris(){
     document.getElementById("ventanaTetris").style.display="block";
 }
+
 function cerrarTetris(){
     document.getElementById("ventanaTetris").style.display="none";
 }
+/*
+$("html").click(function(){
+    $("#div_a_cerrar").hide();
+});
+
+$("#div_a_cerrar").click(function(e){
+    e.stopPropagation();
+});
+*/
